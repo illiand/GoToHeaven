1191f6bcc87284be5eaee49cb3cee87bf0d79969
Ili <sion9801@gmail.com>
2022-03-20 20:23:04 -0400

    commit

---
+++ GoToHeaven/Source/GoToHeaven/GoToHeavenGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GoToHeavenGameMode.h"
+#include "GoToHeavenHUD.h"
+#include "GoToHeavenCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AGoToHeavenGameMode::AGoToHeavenGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AGoToHeavenHUD::StaticClass();
+}
---
+++ GoToHeaven/Source/GoToHeaven/GoToHeavenGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GoToHeavenGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGoToHeavenGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGoToHeavenGameMode();
+};
+
+
+
